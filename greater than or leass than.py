7a5f339e190b6824bf5925d7bb1ce0af75ccf262
Diya2652010 <preetha.moorthy@gmail.com>
2025-03-22 10:51:24 +0530

    greater or less than

---
+++ greater than or leass than.py
@@ -0,0 +1,12 @@
+num=int(input("Enter number to check:"))
+
+if num>50:
+    print("Number is greater than 50")
+    if num%2==0:
+        print("The number is aslo an even number")
+        
+    else:
+        print("The number is also an odd number")
+
+else:
+ print("Number is less than 50")
