a49cef93b0395b2af73ec5b0844253956a75e7f2
Diya2652010 <preetha.moorthy@gmail.com>
2025-03-22 10:28:31 +0530

    even or odd

---
+++ if and else.py
@@ -0,0 +1,8 @@
+number=int(input("Enter The Number To Be Checked:"))
+print("The number to be checked is:",number)
+
+if number%2==0:
+    print("The number entered is an even number")
+
+else:
+    print("The number entered is an odd number")
